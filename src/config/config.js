b42668e3cbc85aca062aa5a187dc209882473bdd
diamond_dai <diamond.dai@gmail.com>
2025-04-16 23:13:22 -0600

    try private IP

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const BackendPort = '8080';
-export const BackendUrl = 'http://localhost';
+export const BackendUrl = 'http://ip-172-31-12-56.us-west-1.compute.internal';
 export const tLimit = 60;
