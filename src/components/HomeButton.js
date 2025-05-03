ae9f17f47c21195d921cd1d0e47bd6fd590ecf7e
diamond_dai <diamond.dai@gmail.com>
2025-05-02 23:22:17 -0600

    Developed the homebutton component

---
+++ src/components/HomeButton.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import home from "../static/img/home.png";
+
+export default function HomeButton(){
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-2">       
+            <div className="group rounded-lg border border-transparent px-5 py-4 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 hover:border-cyan-500 hover:bg-cyan-200/50">
+                <Link className="flex flex-col items-center" to="/" >              
+                    <img className="rounded-md opacity-0 object-cover ease-in-out duration-300 group-hover:-rotate-12 group-hover:scale-125 group-hover:opacity-40 px-3 py-2" 
+                        src={ home } 
+                        width={120} 
+                        height={120} 
+                        alt="Home"/>
+                    <h2 className={`mb-3 text-3xl font-bold text-cyan-700 p-4`}> 
+                        { "Home"}
+                    </h2>
+                </Link>
+            </div>
+        </div>
+    );
+}
