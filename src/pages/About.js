fad8499d1a496b90cd0f324849557feb2f6162a8
diamond_dai <diamond.dai@gmail.com>
2025-06-16 00:31:35 -0600

    Make About page to be responsive to mobile phone view

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,53 +25,45 @@ export default function About() {
                 <ScrollContainer>
                     <ScrollPage>
                         <Animator animation={FadeMoveOut}>
-                            <div className='flex flex-row items-center'>
-                                <div className='text-3xl font-bold p-3 m-3'>About This Web</div>
+                            <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
+                                <div className='flex-1 text-2xl sm:text-3xl font-bold p-3 m-3 text-center sm:text-left'>
+                                    About This Web
+                                </div>
                                 <img src={ web_img } alt="About web"
-                                    width={250}
-                                    height={250}/>
+                                    className="flex-shrink-0 w-32 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
                             </div>
                         </Animator>
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={FadeMoveOut}>
-                            <div className='flex flex-row items-center'>
+                            <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
                                 <img src={ covid_img } alt="Covid"
-                                        width={250}
-                                        height={250}/>
-                                <div>
-                                    <div className='text-2xl font-bold p-3 m-3'>
-                                        The origin of this web came to me in 2021.
-                                    </div>
-                                    <div className='text-2xl font-bold p-3 m-3'>
-                                        During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. 
-                                    </div>
-                                    <div className='text-2xl font-bold p-3 m-3'>
-                                        This really affected our daily lives, including my church activities.
-                                    </div>
+                                    className="flex-0 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
+                                <div className='flex-1 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
+                                    The origin of this web came to me in 2021. <br />
+                                    During that year, we were still facing the threat of COVID-19, which led to bans on large gatherings. <br />
+                                    This really affected our daily lives, including my church activities.
                                 </div>
                             </div>                       
                         </Animator>
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={FadeMoveOut}>
-                            <div className='flex flex-row items-center'>
-                                <div className='text-2xl font-bold p-3 m-3'>
+                            <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
+                                <div className='flex-0 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
                                     At that time, my pastor was looking for interactive and creative ways to read the Bible with our church community.
                                 </div>
                                 <img src={ guide_img } alt="Guide"
-                                    width={250}
-                                    height={250}/>
+                                    className="flex-1 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
                             </div>
                         </Animator>
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={ZoomInScrollOut}>
-                            <div className='flex flex-row items-center'>
+                            <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
                             <img src={ inspire_img } alt="Inspire"
-                                    width={250}
-                                    height={250}/>
-                                <div className='text-2xl font-bold p-3 m-3'>
+                                    className="flex-0 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
+                                <div className='flex-1 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
                                     Inspired by an online game featuring true or false mini-games, I suggested to my pastor that I could create a Bible verse true or false game for our church group.
                                 </div>
                             </div>
@@ -79,43 +71,35 @@ export default function About() {
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={MoveInOut}>
-                            <div className='flex flex-row items-center'>
-                                <div>
-                                    <div className='text-2xl font-bold p-3 m-3'>
-                                        The early stages of development were quite challenging since I did not have any prior experience in web development.
-                                    </div>
-                                    <div className='text-2xl font-bold p-3 m-3'>
-                                        I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database.
-                                    </div>
+                            <div className='flex flex-col sm:flex-row items-center gap-2 sm:gap-4'>
+                                <div className='flex-0 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
+                                    The early stages of development were quite challenging since I did not have any prior experience in web development. <br />
+                                    I learned about Google App Scripts through YouTube and tried to implement it using Google Drive and Google Sheets as a database.
                                 </div>
                                 <img src={ struggle_img } alt="Struggling"
-                                        width={250}
-                                        height={250}/>
+                                    className="flex-1 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
                             </div>                        
                         </Animator>
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={FadeMoveOut}>
-                            <div className='flex flex-row items-center'>
+                            <div className='flex flex-col sm:flex-row items-center gap-3 sm:gap-3'>
                                 <img src={ enjoy_img } alt="Enjoy"
-                                            width={250}
-                                            height={250}/>
-                                <div className='text-2xl font-bold p-3 m-3'>
+                                    className="flex-0 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
+                                <div className='flex-1 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
                                     Eventually, I was able to launch a series of Bible verse OX games, which my church group really enjoyed playing.
                                 </div>
                                 <img src={ trueOrFalse_img } alt="TrueFalse"
-                                            width={250}
-                                            height={250}/>
+                                    className="flex-2 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
                             </div>                        
                         </Animator>
                     </ScrollPage>
                     <ScrollPage>
                         <Animator animation={FadeMoveOut}>
-                            <div className='flex flex-row items-center'>
+                            <div className='flex flex-col sm:flex-row items-center gap-3 sm:gap-3'>
                                 <img src={ bauhinia_img } alt="Bauhinia"
-                                                width={200}
-                                                height={200}/>
-                                <div className='text-2xl font-bold p-3 m-3'>
+                                    className="flex-0 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
+                                <div className='flex-1 text-2xl sm:text-3xl font-bold p-1 text-center sm:text-left'>
                                     Based on the 
                                     <a className='text-3xl font-bold p-1 m-1 text-blue-600 text-decoration-line: underline' 
                                         href="https://sites.google.com/view/sobibleox/Home">
@@ -124,8 +108,7 @@ export default function About() {
                                     . I have added two more features: a Hong Kong OX game and a Canada OX game. 
                                 </div>
                                 <img src={ maple_img } alt="Maple"
-                                                width={200}
-                                                height={200}/>
+                                    className="flex-2 w-40 sm:w-48 md:w-48 max-w-full h-auto px-1 py-1 sm:px-3 sm:py-2"/>
                             </div>            
                             
                         </Animator>
