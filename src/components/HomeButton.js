aa283d15aec2d2a20b2b7df1a547da358ee67561
diamond_dai <diamond.dai@gmail.com>
2025-06-16 00:32:13 -0600

    Standardize button components

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -8,8 +8,6 @@ export default function HomeButton(){
                 <Link className="flex flex-col items-center" to="/" >              
                     <img className="rounded-md opacity-0 object-cover ease-in-out duration-300 group-hover:-rotate-12 group-hover:scale-120 group-hover:opacity-40 w-24 sm:w-32 md:w-40 max-w-full h-auto px-2 py-1 sm:px-3 sm:py-2" 
                         src={ home } 
-                        // width={120} 
-                        // height={120} 
                         alt="Home"/>
                     <h2 className={`mb-3 text-2xl sm:text-3xl font-bold text-cyan-700 p-3`}> 
                         { "Home"}
